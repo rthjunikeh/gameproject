e8b631ed1f2aef6b2693a5ce77af73f9e5c79d9a
rthjunikeh <rthjunikeh@gmail.com>
2024-07-22 12:07:12 +0700

    Initial commit for GameProject

---
+++ src/main/java/com/mycompany/gameproject/NumberGuessingGame.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gameproject;
+
+/**
+ *
+ * @author Asus
+ */
+class NumberGuessingGame extends Game {
+    public NumberGuessingGame(int guessLimit) {
+        super(guessLimit);
+    }
+
+    @Override
+    public void play() {
+        super.play(); // Memanggil metode dari kelas base
+        System.out.println("Terima kasih sudah bermain!");
+    }
+}
